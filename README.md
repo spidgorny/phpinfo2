d08139adf174cff8c9e8b2418f816820ad55852e
DEPIDSVY <depidsvy@nintendo.de>
2021-09-13 15:50:57 +0200

    - add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Improved phpinfo()
+
+![screenshot](docs/Screenshot-2021-09-13-154859.png)
--- src/PhpInfoController.php
+++ src/PhpInfoController.php
@@ -13,11 +13,12 @@ class PhpInfoController
 //		error_log(strlen($info));
 
 		$dom = new Dom();
-		$dom->loadStr($info);
+		$dom->loadStr('<div>' . $info . '</div>');
 		$style = $dom->find('style');
 		if ($style->count()) {
 			$style->delete();
 		}
+
 		$h2 = $dom->find('h2 a');
 		foreach ($h2 as $tag) {
 			$name = $tag->innerHtml();
@@ -25,11 +26,25 @@ class PhpInfoController
 			$menu[] = "<a href='#${slug}'>${name}</a>";
 		}
 
+		$logo = $dom->find('img');
+		$logo->delete();
+
+		$title = $dom->find('h1');
+		$title->delete();
+		$title->setAttribute('class', 'text-center');
+
+		$tables = $dom->find('table');
+		$tables->each(function ($table) {
+			$table->setAttribute('class', 'table');
+		});
+
 		$menu = implode('<br/>', $menu);
 
 		$template = file_get_contents(__DIR__ . '/../template/template.phtml');
 		$template = str_replace('${menu}', $menu, $template);
-		$template = str_replace('${content}', $info, $template);
+		$template = str_replace('${content}', $dom, $template);
+		$template = str_replace('${logo}', $logo, $template);
+		$template = str_replace('${title}', $title, $template);
 		return $template;
 	}
 
